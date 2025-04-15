e0ecc798967b6ad379015020b3955cdb606862dd
AnkitBudhwar7 <ankit.budhwar@edu.sait.ca>
2025-04-15 17:48:34 -0600

    js

--- script/script.js
+++ script/script.js
@@ -1,24 +1,21 @@
-// Function to show an alert message
+
 function showMessage() {
     alert('Welcome to our family');
 }
 
-// Hover effect when mouse is over the button
-document.getElementById('hoverbutton').addEventListener('mouseover', function () {
-    document.getElementById('hoverbutton').style.background =     'grey';
-});
 
-// Revert the button background when mouse leaves
-document.getElementById('hoverbutton').addEventListener('mouseleave', function () {
+document.getElementById('hoverbutton').addEventListener('mouseover'), function () {
+    document.getElementById('hoverbutton').style.background = 'grey';
+}
+document.getElementById('hoverbutton').addEventListener('mouseleave'), function () {
     document.getElementById('hoverbutton').style.background = 'red';
-})
+}
 
+   
 let count = 0;
-document.getElementById('buttoncounter').onclick = function() {
-     count = count + 1;
-     document.getElementById('counter').innerHTML = 'you clicked this '+ count+ 'times';
-     if
-    }
-
-
-
+    document.getElementById('buttoncounter').onclick = function() {
+         count = count + 1;
+         document.getElementById('counter').innerHTML = 'you clicked this '+ count+ 'times';
+         if
+        }
+    
