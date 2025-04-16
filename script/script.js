40271c4632d132fef755e0dd813aaa965f31e5cd
AnkitBudhwar7 <ankit.budhwar@edu.sait.ca>
2025-04-15 20:52:34 -0600

    js

--- script/script.js
+++ script/script.js
@@ -1,21 +1,29 @@
+document.getElementById("alertMe").onclick=function () {
+    alert ("Welcome to our restaurant");
+};
 
-function showMessage() {
-    alert('Welcome to our family');
+document.getElementById("hoverbutton").onmouseover= function () {
+    document.getElementById("hoverbutton").style.background= "grey";
+};
+
+document.getElementById("hoverbutton").onmouseleave=function () { 
+    document.getElementById("hoverbutton").style.background= "yellow";
+};
+
+let count= 0;
+document.getElementById("buttoncounter").onclick=function() { 
+    count= count + 1 ;
+    const countertext = document.getElementById("counter")
+    countertext.innerHTML= "You clicked this"+ count+ "times;"
+
+    if (count %2==0) {
+        countertext.style.color="green"
+    } else {
+        countertext.style.color="blue"
+    }
+};
+
+for (let i=1; i <=5; i++) {
+    document.getElementById("contaner").append("This message number is "+1+".");
 }
 
-
-document.getElementById('hoverbutton').addEventListener('mouseover'), function () {
-    document.getElementById('hoverbutton').style.background = 'grey';
-}
-document.getElementById('hoverbutton').addEventListener('mouseleave'), function () {
-    document.getElementById('hoverbutton').style.background = 'red';
-}
-
-   
-let count = 0;
-    document.getElementById('buttoncounter').onclick = function() {
-         count = count + 1;
-         document.getElementById('counter').innerHTML = 'you clicked this '+ count+ 'times';
-         if
-        }
-    
