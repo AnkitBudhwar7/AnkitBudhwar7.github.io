38d1d50d59e3a028f227465f42785b86016e00ae
AnkitBudhwar7 <ankit.budhwar@edu.sait.ca>
2025-04-15 22:26:03 -0600

    js

--- script/script.js
+++ script/script.js
@@ -1,29 +1,26 @@
-document.getElementById("alertMe").onclick=function () {
-    alert ("Welcome to our restaurant");
+document.getElementById("hoverButton").onmouseover = function () {
+    document.getElementById("hoverButton").style.background = "grey";
 };
 
-document.getElementById("hoverbutton").onmouseover= function () {
-    document.getElementById("hoverbutton").style.background= "grey";
+document.getElementById("hoverButton").onmouseleave = function () {
+    document.getElementById("hoverButton").style.background = "yellow";
 };
 
-document.getElementById("hoverbutton").onmouseleave=function () { 
-    document.getElementById("hoverbutton").style.background= "yellow";
-};
+let count = 0;
+document.getElementById("buttonCounter").onclick = function () {
+    count++;
+    const countertext = document.getElementById("counter");
+    countertext.innerHTML = "You clicked this " + count + " times.";
 
-let count= 0;
-document.getElementById("buttoncounter").onclick=function() { 
-    count= count + 1 ;
-    const countertext = document.getElementById("counter")
-    countertext.innerHTML= "You clicked this"+ count+ "times;"
-
-    if (count %2==0) {
-        countertext.style.color="green"
+    if (count % 2 === 0) {
+        countertext.style.color = "green";
     } else {
-        countertext.style.color="blue"
+        countertext.style.color = "blue";
     }
 };
 
-for (let i=1; i <=5; i++) {
-    document.getElementById("contaner").append("This message number is "+1+".");
+for (let i = 1; i <= 5; i++) {
+    const msg = document.createElement("p");
+    msg.textContent = "This message number is " + i + ".";
+    document.getElementById("container").appendChild(msg);
 }
-
